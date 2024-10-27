99534c6e389fb19187c8793c74df48f77ee91484
ricE06 <ezzhaneri06@gmail.com>
2024-10-27 18:28:45 -0400

    used APIView instead of function based api

--- fullhouse-backend/accounts/views.py
+++ fullhouse-backend/accounts/views.py
@@ -5,6 +5,9 @@ from django.contrib import messages
 from django.core import serializers
 from rest_framework import generics
 from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 from . import models
 from .models import User, Member
 from .serializers import UserSerializer, MemberSerializer, TinySerializer
@@ -14,25 +17,29 @@ from .serializers import UserSerializer, MemberSerializer, TinySerializer
 def login_user(request):
     return render(request, "login.html", {})
 
-def user_profile(request):
+class MemberProfileView(APIView):
     """
     Gets and posts user information for the profile page.
     GET: requires a username attribute
-    POST: requires a JSON file
+    POST: requires a JSON of information to store
     """
-    if request.method == "GET":
-        # we need a request.username attribute to get out the user
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, format=None):
         request_data = JSONParser().parse(request)
         out_user = User.objects.get(username=request_data.username)
         out_member = Member.objects.get(user=out_user)
-        return serializers.serialize("json", out_member)
-    elif request.method == "POST":
+        return JsonResponse(serializers.serialize("json", out_member))
+
+    def post(self, request, format=None):
         user_data = JSONParser().parse(request)
         serializer = MemberSerializer(data=user_data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
 
 def test_tiny(request):
     """
