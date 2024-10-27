346df4025f5a5289772ecc9df09552bc640c797f
ricE06 <ezzhaneri06@gmail.com>
2024-10-27 18:42:10 -0400

    made urls route to APIView class

--- fullhouse-backend/accounts/urls.py
+++ fullhouse-backend/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from accounts.views import login_user, user_profile, test_tiny
+from accounts import views
 
 urlpatterns = [
     path('api/login_user', login_user, name='login_user'),
-    path('api/user_profile', user_profile, name='user_profile'),
+    path('api/auth/', views.MemberProfileView.as_view()),
     path('api/test_tiny', test_tiny, name='test_tiny'),
 ]
