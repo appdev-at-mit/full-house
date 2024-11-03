37d8f93a08a337f9a7c7a041e807829810763450
rahsunk <rtkf0522@gmail.com>
2024-11-03 18:24:16 -0500

    Updated Tailwind CSS

--- fullhouse-frontend/app/(default)/otherProfile/page.tsx
+++ fullhouse-frontend/app/(default)/otherProfile/page.tsx
@@ -5,16 +5,17 @@ import Image from "next/image";
 export default function OtherProfile() {
     // Replace these constants with data from backend
     const otherPerson = "Rahsun";
-    const bio = "lorem ipsum"
+    const bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     const location = "Cambridge MA"
     const phone = "555-123-4567"
     const email = "rahsunk@mit.edu"
 
     return(
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <div>
-                <div className="p-10 bg-white rounded-lg shadow-md">
-                    <div className="flex justify-around">
+            <div className="flex flex-col justify-between m-10">
+                <div className="p-20 bg-white rounded-lg shadow-md">
+                    <div className="flex justify-around m-5 space-x-10">
+                            {/* Replace this temporary image with profile picture of the user (from backend) */}
                         <Image
                         src="/kirbyPop.jpg"
                         alt="Profile Picture"
@@ -23,22 +24,22 @@ export default function OtherProfile() {
                         />
                         <div>
                             <h1 className="text-4xl font-bold mb-2">Hi! I'm {otherPerson}</h1>
-                            <p>{bio}</p>
+                            <p className="">{bio}</p>
                         </div>
                     </div>
-                    <div>
+                    <div className="m-5">
                         <p>I am staying at {location} this summer.</p>
                         <p>Phone: {phone}</p>
                         <p>Email: {email}</p>
-                        <Button variant="default">
+                        <Button variant="default" className="my-3">
                             Message
                         </Button>
                     </div>
                 </div>
-                <div>
-                    <h1>Other people in the same area:</h1>
-                    {/* Replce using components and a list of people*/}
-                    <div className="flex flex-row">
+                <div className="my-5">
+                    <h1 className="my-5">Other people in the same area:</h1>
+                    {/* Replce using components and a list of people; for now I have four duplicate users as an example*/}
+                    <div className="flex flex-row grid grid-cols-3 gap-4">
                         <div className="p-10 bg-white rounded-lg shadow-md">
                             <div className="flex justify-around">
                                 <Image
@@ -49,9 +50,9 @@ export default function OtherProfile() {
                                 />
                                 <h1 className="text-4xl font-bold mb-2">{otherPerson}</h1>
                             </div>
-                            <div>
+                            <div className="m-5 my-3">
                                 <p>Staying at {location} this summer.</p>
-                                <Button variant="default">
+                                <Button variant="default" className="my-3">
                                     View Profile
                                 </Button>
                             </div>
@@ -66,9 +67,43 @@ export default function OtherProfile() {
                                 />
                                 <h1 className="text-4xl font-bold mb-2">{otherPerson}</h1>
                             </div>
-                            <div>
+                            <div className="m-5 my-3">
                                 <p>Staying at {location} this summer.</p>
-                                <Button variant="default">
+                                <Button variant="default" className="my-3">
+                                    View Profile
+                                </Button>
+                            </div>
+                        </div>
+                        <div className="p-10 bg-white rounded-lg shadow-md">
+                            <div className="flex justify-around">
+                                <Image
+                                src="/kirbyPop.jpg"
+                                alt="Profile Picture"
+                                width={128}
+                                height={128}
+                                />
+                                <h1 className="text-4xl font-bold mb-2">{otherPerson}</h1>
+                            </div>
+                            <div className="m-5 my-3">
+                                <p>Staying at {location} this summer.</p>
+                                <Button variant="default" className="my-3">
+                                    View Profile
+                                </Button>
+                            </div>
+                        </div>
+                        <div className="p-10 bg-white rounded-lg shadow-md">
+                            <div className="flex justify-around">
+                                <Image
+                                src="/kirbyPop.jpg"
+                                alt="Profile Picture"
+                                width={128}
+                                height={128}
+                                />
+                                <h1 className="text-4xl font-bold mb-2">{otherPerson}</h1>
+                            </div>
+                            <div className="m-5 my-3">
+                                <p>Staying at {location} this summer.</p>
+                                <Button variant="default" className="my-3">
                                     View Profile
                                 </Button>
                             </div>
