6574eaa3123738c0ed4c90578c0a45dbbd7b0a31
rahsunk <rtkf0522@gmail.com>
2024-11-23 12:17:41 -0500

    Created otherUser component

--- fullhouse-frontend/app/(default)/otherProfile/page.tsx
+++ fullhouse-frontend/app/(default)/otherProfile/page.tsx
@@ -21,7 +21,27 @@ export default function OtherProfile() {
         guestPolicy: "Spontaneity is great! Anything (within reason) is fine by me.",
         sleepLightLevel: "No preference",
     }
-    // const router = useRouter();
+
+    function mockUser(){
+        return <div className="p-10 bg-white rounded-lg shadow-md">
+            <div className="flex justify-around space-x-5 m-3">
+                <Image
+                src="/kirbyPop.jpg"
+                alt="Profile Picture"
+                width={128}
+                height={128}
+                />
+                <h1 className="text-4xl font-bold mb-2">{otherPerson}</h1>
+            </div>
+            <div className="m-5 my-3">
+                <p>Staying at {location} this summer.</p>
+                <Button variant="default" className="my-3">
+                    View Profile
+                </Button>
+            </div>
+        </div>
+    }
+
 
     return(
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
@@ -63,74 +83,10 @@ export default function OtherProfile() {
                     <h2 className="text-2xl font-bold mb-2 my-5">Other people in the same area:</h2>
                     {/* Replce using components and a list of people; for now I have four duplicate users as an example*/}
                     <div className="flex flex-row grid grid-cols-3 gap-4">
-                        <div className="p-10 bg-white rounded-lg shadow-md">
-                            <div className="flex justify-around space-x-5 m-3">
-                                <Image
-                                src="/kirbyPop.jpg"
-                                alt="Profile Picture"
-                                width={128}
-                                height={128}
-                                />
-                                <h1 className="text-4xl font-bold mb-2">{otherPerson}</h1>
-                            </div>
-                            <div className="m-5 my-3">
-                                <p>Staying at {location} this summer.</p>
-                                <Button variant="default" className="my-3">
-                                    View Profile
-                                </Button>
-                            </div>
-                        </div>
-                        <div className="p-10 bg-white rounded-lg shadow-md">
-                            <div className="flex justify-around space-x-5 m-3">
-                                <Image
-                                src="/kirbyPop.jpg"
-                                alt="Profile Picture"
-                                width={128}
-                                height={128}
-                                />
-                                <h1 className="text-4xl font-bold mb-2">{otherPerson}</h1>
-                            </div>
-                            <div className="m-5 my-3">
-                                <p>Staying at {location} this summer.</p>
-                                <Button variant="default" className="my-3">
-                                    View Profile
-                                </Button>
-                            </div>
-                        </div>
-                        <div className="p-10 bg-white rounded-lg shadow-md">
-                            <div className="flex justify-around space-x-5 m-3">
-                                <Image
-                                src="/kirbyPop.jpg"
-                                alt="Profile Picture"
-                                width={128}
-                                height={128}
-                                />
-                                <h1 className="text-4xl font-bold mb-2">{otherPerson}</h1>
-                            </div>
-                            <div className="m-5 my-3">
-                                <p>Staying at {location} this summer.</p>
-                                <Button variant="default" className="my-3">
-                                    View Profile
-                                </Button>
-                            </div>
-                        </div>
-                        <div className="p-10 bg-white rounded-lg shadow-md">
-                            <div className="flex justify-around space-x-5 m-3">
-                                <Image
-                                src="/kirbyPop.jpg"
-                                alt="Profile Picture"
-                                width={128}
-                                height={128}
-                                />
-                                <h1 className="text-4xl font-bold mb-2">{otherPerson}</h1>
-                            </div>
-                            <div className="m-5 my-3">
-                                <p>Staying at {location} this summer.</p>
-                                <Button variant="default" className="my-3">
-                                    View Profile
-                                </Button>
-                            </div>
-                        </div>
+                        {mockUser()}
+                        {mockUser()}
+                        {mockUser()}
+                        {mockUser()}
                     </div>
                 </div>
             </div>
